895531b277901e1da04749f9d5442db064be8597
khorevaa <khorevaa@gmail.com>
2017-12-19 18:41:35 +0300

    Поравка скрипта

--- deploy-hub.oscript.sh
+++ deploy-hub.oscript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-export CHANNEL=dev
-if [ $TRAVIS_BRANCH == "master" ]; then export CHANNEL=stable;fi
+export CHANNEL=dev;
+if [ $TRAVIS_BRANCH == 'master' ]; then export CHANNEL=stable;fi
 temp=`cat packagedef | grep ".Версия(" | sed 's|[^"]*"||' | sed -r 's/".+//'`;
-export VERSION=${temp##*|}; 
-oscript /usr/share/oscript/lib/opm/src/opm.os push --token $GITHUB_OAUTH_TOKEN --channel $CHANNEL --file ./cli-$VERSION.ospx;
+export LIB_VERSION=${temp##*|}; 
+echo $LIB_VERSION;
+oscript /usr/share/oscript/lib/opm/src/opm.os push --token $GITHUB_OAUTH_TOKEN --channel $CHANNEL --file ./cli-$LIB_VERSION.ospx;
