01c4e650f70fe4aef0a62218a4168dfcd3c5b7dc
khorevaa <khorevaa@gmail.com>
2017-12-19 18:25:16 +0300

    Добавил определение канала публикации

--- deploy-hub.oscript.sh
+++ deploy-hub.oscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
+export CHANNEL=dev
+if [ $TRAVIS_BRANCH == "master" ]; then export CHANNEL=stable;fi
 temp=`cat packagedef | grep ".Версия(" | sed 's|[^"]*"||' | sed -r 's/".+//'`;
 export VERSION=${temp##*|}; 
-oscript /usr/share/oscript/lib/opm/src/opm.os push --token $GITHUB_OAUTH_TOKEN --channel stable --file ./cli-$VERSION.ospx;
+oscript /usr/share/oscript/lib/opm/src/opm.os push --token $GITHUB_OAUTH_TOKEN --channel $CHANNEL --file ./cli-$VERSION.ospx;
