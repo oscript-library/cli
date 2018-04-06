1aa93b607a896b877e8ea4d233252133fca820b7
khorevaa <Khorevaa@gmail.com>
2018-04-06 16:42:26 +0300

    SonarFix

--- sonar-qube.sh
+++ sonar-qube.sh
@@ -4,7 +4,7 @@ version=${temp##*|}
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     sonar-scanner \
-        -Dsonar.host.url=http://sonar.silverbulleters.org \
+        -Dsonar.host.url=https://sonar.silverbulleters.org \
         -Dsonar.analysis.mode=issues \
         -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
@@ -14,7 +14,7 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
 
   elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     sonar-scanner \
-        -Dsonar.host.url=http://sonar.silverbulleters.org \
+        -Dsonar.host.url=https://sonar.silverbulleters.org \
         -Dsonar.login=$SONAR_TOKEN \
         -Dsonar.projectVersion=$version\
         -Dsonar.scanner.skip=false
