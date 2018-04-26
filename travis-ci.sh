f3a76cca30a1a51244e27f764d11afddf20c1533
Aleksey Khorev <665525+khorevaa@users.noreply.github.com>
2018-04-26 21:44:22 +0300

    Update travis-ci.sh

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,9 +1,15 @@
+set -e
+echo "Устанавливаю версию OScript <$OSCRIPT_VERSION>"
+curl http://oscript.io/downloads/$OSCRIPT_VERSION/deb > oscript.deb 
+dpkg -i oscript.deb 
+rm -f oscript.deb
+
 opm install 1testrunner; 
 opm install 1bdd; 
 
 opm install; 
 
-opm run test;
+opm run coverage;
 
 
 # oscript ./tasks/coverage.os
