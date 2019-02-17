1627dc99c5d5df2c55642f19c07f641cdbe1a3d7
Aleksey Khorev <665525+khorevaa@users.noreply.github.com>
2019-02-17 19:31:33 +0300

    Update sonar-qube.sh

--- sonar-qube.sh
+++ sonar-qube.sh
@@ -1,8 +1,10 @@
 temp=`cat packagedef | grep ".Версия(" | sed 's|[^"]*"||' | sed -r 's/".+//'`
 version=${temp##*|}
-
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    wget -O bsl-language-server.jar https://github.com/1c-syntax/bsl-language-server/releases/download/v0.3.0/bsl-language-server-0.3.0.jar
+    java -jar ./bsl-language-server.jar -a -s ./src -r json
+    
     sonar-scanner \
         -Dsonar.host.url=https://sonar.silverbulleters.org \
         -Dsonar.analysis.mode=issues \
@@ -13,6 +15,8 @@ if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
         -Dsonar.scanner.skip=false
 
   elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    wget -O bsl-language-server.jar https://github.com/1c-syntax/bsl-language-server/releases/download/v0.3.0/bsl-language-server-0.3.0.jar
+    java -jar ./bsl-language-server.jar -a -s ./src -r json
     sonar-scanner \
         -Dsonar.host.url=https://sonar.oscript.ru \
         -Dsonar.login=$SONAR_TOKEN \
