34c246298a948a27d0686f71ea2e50486253836c
Aleksey Khorev <horevaa@yandex.ru>
2020-01-23 20:36:40 +0300

    Починка тестов на ci

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,6 +1,6 @@
 set -e
 echo "Устанавливаю версию OScript <$OSCRIPT_VERSION>"
-curl http://oscript.io/downloads/$OSCRIPT_VERSION/deb > oscript.deb 
+curl -L http://oscript.io/downloads/$OSCRIPT_VERSION/deb > oscript.deb 
 dpkg -i oscript.deb 
 rm -f oscript.deb
 
